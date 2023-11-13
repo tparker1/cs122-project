4361002a7f55111b225cf7849d033733fc5850d4
Ezra Hsieh <ezrahsieh@yahoo.com>
2023-11-13 14:27:57 -0800

    added header and dropdown menu

--- box_office_analysis.py
+++ box_office_analysis.py
@@ -9,3 +9,30 @@ from flask import request
 
 # import csv module
 import csv
+
+# create a Flask object called app
+app = Flask(__name__)
+
+# Read data from CSV file and store it in a list of dictionaries
+# with open('weekly.csv') as file:
+#     csv_reader = csv.DictReader(file)
+#     weekly = list(csv_reader)
+
+# define a route to the home page
+# create a home function
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('box_office_home.html')
+
+# define a route to the year page
+# add 'GET' to the methods
+# create a year function
+@app.route("/year", methods=['GET'])
+def year():
+    pass
+
+# add a main method to run the app
+# as a typical Python script
+if __name__ == '__main__':
+    app.run()
