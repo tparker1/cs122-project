47a4af0fcaeaf0fd3774e13e99adbf58624934a2
Ezra Hsieh <ezrahsieh@yahoo.com>
2023-11-13 13:51:59 -0800

    created box_office_analysis.py

---
+++ box_office_analysis.py
@@ -0,0 +1,11 @@
+# import the Flask class from the flask module
+from flask import Flask
+
+# import the render_template function
+from flask import render_template
+
+# import the request function
+from flask import request
+
+# import csv module
+import csv
