9607239290c85a9df00848fbbe2ddbb8f510b062
Tara Parker <tara.parker@sjsu.edu>
2023-11-13 14:44:24 -0800

    Functions to get fetch and plot daily box office data

---
+++ get_daily_data.py
@@ -0,0 +1,101 @@
+# import the requests and BeautifulSoup modules
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+
+def get_daily_data_for_year(year="2023"):
+    # Path to the daily box office data, per year
+    url = "https://www.boxofficemojo.com/daily/"+ year+ "/?view=year"
+
+    # get the soup with beautiful soup
+    req=requests.get(url)
+    content=req.text
+    soup=BeautifulSoup(content)
+    rows=soup.findAll('tr')
+    
+
+    appended_data = []
+    for row in rows:
+        data_row = {}
+        data = row.findAll('td')
+        if len(data) == 0:
+            continue
+        if len(data[0].findAll('span')) > 0:
+            #special weekend
+            data_row['Occasion'] = data[0].findAll('span')[0].text
+            data_row['Date'] = data[0].findAll('a')[0].text
+        else:
+            #normal weekend
+            data_row['Occasion'] = ""
+            data_row['Date'] = data[0].text
+        if len(data) == 0:
+            continue
+
+        data_row['Day'] = data[1].text
+        data_row['Day#'] = data[2].text
+        data_row['Top10Gross'] = data[3].text
+        data_row['PercentChangeYD'] = data[4].text
+        data_row['PercentChangeLW'] = data[5].text
+        data_row['Releases'] = data[6].text
+        data_row['Number1Release'] = data[7].text
+        data_row['Gross'] = data[8].text
+
+        appended_data.append(data_row)
+
+        df = clean_pd_data(appended_data, year)
+
+    return df
+
+
+def convert_to_datetime(date_str, year_str):
+    # Combine the date string and the year string
+    combined_str = f'{date_str} {year_str}'
+
+    # Convert the combined string into a datetime object
+    datetime_obj = datetime.strptime(combined_str, '%b %d %Y')
+
+    return datetime_obj
+
+
+def clean_pd_data(appended_data, year):
+    df = pd.DataFrame(appended_data, columns = ['Occasion', 'Date', 'Day', 'Day#', 'Top10Gross', 'PercentChangeYD', 'PercentChangeLW', 'Releases', 'Number1Release', 'Gross'])
+
+    # fix data types for each column in the dataframe
+    df['Day'] = df['Day'].astype(str)
+    df['Day#'] = df['Day#'].astype(int)
+    df['Releases'] = df['Releases'].astype(int)
+    df['Number1Release'] = df['Number1Release'].astype(str)
+
+    df['Gross'] = df['Gross'].str.replace('$', '')
+    df['Gross'] = df['Gross'].str.replace(',', '')
+    df['Gross'] = df['Gross'].astype(int)
+
+    df['Top10Gross'] = df['Top10Gross'].str.replace('$', '')
+    df['Top10Gross'] = df['Top10Gross'].str.replace(',', '')
+    df['Top10Gross'] = df['Top10Gross'].astype(int)
+
+    df['PercentChangeYD'] = df['PercentChangeYD'].astype(str)
+    df['PercentChangeLW'] = df['PercentChangeLW'].astype(str)
+
+    df['Datetime'] = df['Date'].apply(lambda x: convert_to_datetime(x, year))
+
+    return df
+
+
+def plot_daily_data_by_year(df, year):
+    plt.figure(figsize=(20,10))
+    plt.plot(df['Datetime'], df['Gross'])
+    plt.title('Daily Gross for ' + str(year))
+    plt.xlabel('Date')
+    plt.ylabel('Gross')
+
+    # Change the y-axis formatter to display full values
+    plt.gca().get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+
+    plt.xticks(rotation=45)
+    plt.savefig('daily_gross.png')
+    return 
