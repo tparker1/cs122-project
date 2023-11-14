2a8d43ebae5130d82ac0140629869ea52abe861f
Ezra Hsieh <ezrahsieh@yahoo.com>
2023-11-14 00:04:38 -0800

    created save_data.py

---
+++ save_data.py
@@ -0,0 +1,28 @@
+import csv
+import pandas as pd
+import os
+
+import get_data as gd
+
+def save_weekly_data_to_csv():
+    # Identify the years with box office data
+    end_year = 1982
+    data_list = []
+
+    # Scrape all weekly data from 1982 until most recent year
+    while True:
+        try:
+            print('Getting data from year', end_year)
+            df = gd.get_weekly_data_for_year(str(end_year))
+        # break loop when data is not available for this year
+        except ValueError:
+            break
+        
+        csv_filename = str(end_year) + '_weekly.csv'
+        df.to_csv(os.path.join('weekly_csv', csv_filename), index=True)
+        # data_list.append(df)
+
+        end_year += 1
+
+    end_year -= 1
+
