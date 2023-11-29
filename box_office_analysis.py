3cbfac830324c85964f1a86a7ebfce54223c1754
Ezra Hsieh <ezrahsieh@yahoo.com>
2023-11-29 13:57:05 -0800

    Merge branch 'main' of https://github.com/tparker1/cs122-project

--- box_office_analysis.py
+++ box_office_analysis.py
@@ -111,11 +111,10 @@ def pie():
     elif selected_year < min(year_list):
         selected_year = start_year
 
-    # # create pie chart for selected year
-    # current_year_csv_path = os.path.join(directory_path, str(selected_year) + '_weekly.csv')
-    # df = pd.read_csv(current_year_csv_path)
-    # gd.get_top_movies_pie_chart(df, selected_year, 'domestic')
-    gd.run_get_pie_plot(selected_year)
+    # create pie chart for selected year
+    current_year_csv_path = os.path.join(directory_path, str(selected_year) + '_weekly.csv')
+    df = pd.read_csv(current_year_csv_path)
+    gd.get_top_movies_pie_chart(df, selected_year, 'domestic')
 
     return render_template('pie_chart.html', years=year_list, selected_year=selected_year)
 
